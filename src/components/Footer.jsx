2a6b67d76d65c15a8df63f30b6587c8f843abfd9
Uzairkhan011 <uk53286@gmail.com>
2023-02-07 00:25:37 +0500

    formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,27 @@
-
-
-
-function Footer(){
-
-return(
+function Footer() {
+  return (
     <>
-    <footer>
+      <footer>
         <div className="container">
           <div className="row">
             <div className="col-md-4">
               <div className="full">
                 <div className="logo_footer">
-                  <a href="#"><img width={210} src="images/qq.PNG" alt="#" /></a>
+                  <a href="#">
+                    <img width={210} src="images/qq.PNG" alt="#" />
+                  </a>
                 </div>
                 <div className="information_f">
-                  <p><strong>ADDRESS:</strong> 28 White tower, Street Name New York City, USA</p>
-                  <p><strong>TELEPHONE:</strong> +91 987 654 3210</p>
-                  <p><strong>EMAIL:</strong> yourmain@gmail.com</p>
+                  <p>
+                    <strong>ADDRESS:</strong> 28 White tower, Street Name New
+                    York City, USA
+                  </p>
+                  <p>
+                    <strong>TELEPHONE:</strong> +91 987 654 3210
+                  </p>
+                  <p>
+                    <strong>EMAIL:</strong> yourmain@gmail.com
+                  </p>
                 </div>
               </div>
             </div>
@@ -28,12 +33,24 @@ return(
                       <div className="widget_menu">
                         <h3>Menu</h3>
                         <ul>
-                          <li><a href="#">Home</a></li>
-                          <li><a href="#">About</a></li>
-                          <li><a href="#">Services</a></li>
-                          <li><a href="#">Testimonial</a></li>
-                          <li><a href="#">Blog</a></li>
-                          <li><a href="#">Contact</a></li>
+                          <li>
+                            <a href="#">Home</a>
+                          </li>
+                          <li>
+                            <a href="#">About</a>
+                          </li>
+                          <li>
+                            <a href="#">Services</a>
+                          </li>
+                          <li>
+                            <a href="#">Testimonial</a>
+                          </li>
+                          <li>
+                            <a href="#">Blog</a>
+                          </li>
+                          <li>
+                            <a href="#">Contact</a>
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -41,28 +58,46 @@ return(
                       <div className="widget_menu">
                         <h3>Account</h3>
                         <ul>
-                          <li><a href="#">Account</a></li>
-                          <li><a href="#">Checkout</a></li>
-                          <li><a href="#">Login</a></li>
-                          <li><a href="#">Register</a></li>
-                          <li><a href="#">Shopping</a></li>
-                          <li><a href="#">Widget</a></li>
+                          <li>
+                            <a href="#">Account</a>
+                          </li>
+                          <li>
+                            <a href="#">Checkout</a>
+                          </li>
+                          <li>
+                            <a href="#">Login</a>
+                          </li>
+                          <li>
+                            <a href="#">Register</a>
+                          </li>
+                          <li>
+                            <a href="#">Shopping</a>
+                          </li>
+                          <li>
+                            <a href="#">Widget</a>
+                          </li>
                         </ul>
                       </div>
                     </div>
                   </div>
-                </div>     
+                </div>
                 <div className="col-md-5">
                   <div className="widget_menu">
                     <h3>Newsletter</h3>
                     <div className="information_f">
-                      <p>Subscribe by our newsletter and get update protidin.</p>
+                      <p>
+                        Subscribe by our newsletter and get update protidin.
+                      </p>
                     </div>
                     <div className="form_sub">
                       <form>
                         <fieldset>
                           <div className="field">
-                            <input type="email" placeholder="Enter Your Mail" name="email" />
+                            <input
+                              type="email"
+                              placeholder="Enter Your Mail"
+                              name="email"
+                            />
                             <input type="submit" defaultValue="Subscribe" />
                           </div>
                         </fieldset>
@@ -75,10 +110,8 @@ return(
           </div>
         </div>
       </footer>
-    
     </>
-)
-
+  );
 }
 
 export default Footer;
