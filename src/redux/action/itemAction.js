97fc3be3af282d18c77161c286b9a47a44c73758
Uzairkhan011 <uk53286@gmail.com>
2023-02-13 14:37:11 +0500

    payement integration

--- src/redux/action/itemAction.js
+++ src/redux/action/itemAction.js
@@ -1,6 +1,11 @@
 /** @format */
 
-import { DELETE_ITEM, ADD_ITEM, UPDATE_ITEM_QUANTITY } from "./types";
+import {
+  DELETE_ITEM,
+  ADD_ITEM,
+  UPDATE_ITEM_QUANTITY,
+  REMOVE_ALL,
+} from "./types";
 
 // This portion is just for add and delete items from the card
 export const addItems = (item) => {
@@ -9,6 +14,11 @@ export const addItems = (item) => {
 
 export const deleteItems = (id) => {
   return (dispatch) => dispatch({ type: DELETE_ITEM, payload: id });
+};
+
+export const removeAllItems = () => {
+  console.log("I am here....");
+  return (dispatch) => dispatch({ type: REMOVE_ALL });
 };
 
 export const updateItemQuantity = (itemId, quantity) => {
